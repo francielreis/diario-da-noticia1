7fa389806bb49e9d2231de11b48847f1672b58de
francielreis <francielreis739@gmail.com>
2026-08-12 12:38:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def inicio():
     return """
     <h1>Diário da Notícia</h1>
     <p>Bem-vindo ao nosso portal de notícias!</p>
     """
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
